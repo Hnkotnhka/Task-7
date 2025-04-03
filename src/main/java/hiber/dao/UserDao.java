3f20b5a9d87d12ad862c31d0a33d3da3aafe7dba
Hnkotnhka <hnkotnhka@gmail.com>
2025-04-03 13:36:49 +0300

    first commit

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -5,9 +5,13 @@ import hiber.model.User;
 import java.util.List;
 
 public interface UserDao {
+
    void add(User user);
+
    List<User> listUsers();
+
    public User findById(Long id);
+
    void delete(Long id);
 
 }
--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -5,8 +5,12 @@ import hiber.model.User;
 import java.util.List;
 
 public interface UserService {
+
     void add(User user);
+
     public List<User> listUsers();
+
     public User getUserById(Long id);
+
     public void deleteUser(Long id);
 }
